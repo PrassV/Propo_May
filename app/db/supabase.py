6f132a7ea2d6b34c6638707d05e7e2fb7cb2380e
PrassV <prassannavenkat@gmail.com>
2025-05-01 22:45:26 +0530

    commit

--- app/db/supabase.py
+++ app/db/supabase.py
@@ -2,6 +2,6 @@ from supabase import create_client, Client
 from app.core.config.settings import settings
 
 def get_supabase_client() -> Client:
-    return create_client(settings.SUPABASE_URL, settings.SUPABASE_KEY)
+    return create_client(settings.SUPABASE_URL, settings.SUPABASE_ANON_KEY)
 
 supabase = get_supabase_client() 
